f0682afe5d29f7c6e28ab31c0d42d9127f660ed1
manu <emanuele.pocelli@mail.polimi.it>
2026-04-20 01:32:43 +0300

    added locked movement on game start

--- air-hockey/Scripts/paddleMove.gd
+++ air-hockey/Scripts/paddleMove.gd
@@ -2,15 +2,22 @@ extends CharacterBody2D
 
 @export var start_position: Vector2
 @export var player: int
+@onready var collision_shape = $CollisionShape2D
+#variable to lock the paddle movement in point start
+var locked=true
 
 func reset():
+	locked=true
 	position= start_position*player
 	
 	
 func _physics_process(delta):
 	if player==1:
+		var mouse_pos = get_global_mouse_position()
 		if GameState.game_state==GameState.GameStates.PLAYING or GameState.game_state==GameState.GameStates.ENDED:
-			var mouse_pos = get_global_mouse_position()
-			
-			velocity = (mouse_pos - global_position) / delta
-			move_and_slide()
+			if locked:
+				if collision_shape.shape.get_rect().has_point(to_local(mouse_pos)):
+					locked=false
+			else:
+				velocity = (mouse_pos - global_position) / delta
+				move_and_slide()
