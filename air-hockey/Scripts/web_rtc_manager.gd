008dfd94ba83585e6260bcd20c08ba35834fac19
manu <emanuele.pocelli@mail.polimi.it>
2026-05-24 20:10:38 +0300

    updated to hopefully fix web connections

--- air-hockey/Scripts/web_rtc_manager.gd
+++ air-hockey/Scripts/web_rtc_manager.gd
@@ -19,12 +19,12 @@ var _join_sent:         bool   = false
 
 var ICE_SERVERS = {
 	"iceServers": [
-		#{"urls": ["stun:stun.l.google.com:19302"]},
+		{"urls": ["stun:stun.l.google.com:19302"]},
 		{
 			"urls": [
-				"turn:openrelay.metered.ca:80",
-				"turn:openrelay.metered.ca:443",
-                "turn:openrelay.metered.ca:443?transport=tcp"
+				"turn:openrelay.metered.ca:80?transport=tcp",
+				"turn:openrelay.metered.ca:443?transport=tcp",
+                "turns:openrelay.metered.ca:443?transport=tcp"
 			],
 			"username": "openrelayproject",
 			"credential": "openrelayproject"
